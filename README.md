dff73e6da8ac90623f41bcc42b83a0148fc5e866
Guido Gloor <haslo@haslo.ch>
2023-03-03 17:55:04 +0100

    README and footer link to repo added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Code Dojo
+
+Dies ist ein Design speziell für die Webseite https://codedojo.ch.
+
+Gebaut mit Liebe und auf Basis von [Shapely](https://colorlib.com/wp/themes/shapely) und [Bootstrap](https://getbootstrap.com/).
+
+Die Bilder wurden alle erstellt mit [MidJourney](https://www.midjourney.com/).
--- footer.php
+++ footer.php
@@ -26,7 +26,8 @@
                     <?php echo wp_kses_post( get_theme_mod( 'shapely_footer_copyright' ) ); ?>
                 </div>
                 <div class="footer-credits">
-                    Design von <a href="https://haslo.ch/">haslo</a>
+                    Design von <a href="https://haslo.ch/">haslo</a>, und
+                    <a href="https://github.com/haslo/codedojo_theme">hier ist der Code</a>.
                 </div>
             </div><!-- .site-info -->
             <div class="col-sm-6 text-right">
